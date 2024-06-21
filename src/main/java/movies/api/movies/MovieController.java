b532d676408cd78d3032b6dd5ceb2aed33d199b9
Ishita Chauhan <96109876+Ishita-Chauhan@users.noreply.github.com>
2024-06-21 19:49:34 +0530

    Update MovieController.java

--- src/main/java/movies/api/movies/MovieController.java
+++ src/main/java/movies/api/movies/MovieController.java
@@ -3,16 +3,14 @@ package movies.api.movies;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "*")
 @RestController
+
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
